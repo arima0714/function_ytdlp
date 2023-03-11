4f7a0faa61fb1173badb6cf7456d59d35aef7c0f
@rima <32933855+arima0714@users.noreply.github.com>
2023-03-12 00:18:16 +0900

    get minio infos

--- functionytdlp/handler.py
+++ functionytdlp/handler.py
@@ -1,10 +1,23 @@
 from yt_dlp import YoutubeDL
+import boto3
+import os
 
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
+
+    s3 = boto3.resource(
+        's3',
+        endpoint_url = "http://10.1.21.85:9000",
+        aws_access_key_id = os.environ['username'],
+        aws_secret_access_key = os.environ['password']
+    )
+
+    print("all buckets")
+    for bucket in s3.buckets.all():
+        print(bucket.name)
 
     URLS = ["https://www.youtube.com/watch?v=BaW_jenozKc"]
     return req
