b3debc7ad9bb19451e3398c1f4b3af358872574f
@rima <32933855+arima0714@users.noreply.github.com>
2023-03-12 18:25:21 +0900

    add feature about downloading sample video

--- functionytdlp/handler.py
+++ functionytdlp/handler.py
@@ -1,5 +1,6 @@
 from yt_dlp import YoutubeDL
 import boto3
+import botocore
 import os
 import json
 
@@ -20,11 +21,38 @@ def handle(req):
     for bucket in s3.buckets.all():
         print(bucket.name)
 
+    s3_client = boto3.client(
+        "s3",
+        use_ssl=False,
+        endpoint_url = "http://10.1.21.85:9000",
+        aws_access_key_id = os.environ['username'],
+        aws_secret_access_key = os.environ['password']
+    )
+
     URL = "https://www.youtube.com/watch?v=BaW_jenozKc"
     URLS = [URL]
-    with YoutubeDL() as ydl:
+    ydl_opts = {'outtmpl': './resolution'+'%(resolution)s'+'duration'+'%(duration)s'+'_.mp4'}
+    with YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(URL, download=False)
-        print(json.loads(json.dumps(ydl.sanitize_info(info)))["title"])
+        dict_informations = json.loads(json.dumps(ydl.sanitize_info(info)))
+        resolution = dict_informations["resolution"]
+        duration = dict_informations["duration"]
+
         ydl.download(URLS)
+
+    print('botocore vertion is {0}'.format(botocore.__version__))
+    print('boto3 vertion is {0}'.format(boto3.__version__))
+
+    print(f"resolution={resolution}, duration = {duration}")
+    ls_file_name = os.listdir()
+    print(f"{ls_file_name = }")
+    filenames = [s for s in ls_file_name if 'resolution' in s]
+    filename = filenames[0]
+
+    bucketname = "videos"
+    s3_client.upload_file(f"./{filename}", bucketname, filename)
+
+    for i_filename in filenames:
+        os.remove(f"./{i_filename}")
 
     return req
